6c0a8c3b48d1aa77704b19b56d35860a6322694f
Nina <ninarachaebuie@gmail.com>
2019-04-19 08:45:32 -0500

    Updates

---
+++ precache-manifest.3dfa4a8c4bdc190d81dcde3c8fb6ade1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a8b1950a48a4ee96ba53ac7963beb5f",
+    "url": "/egg-move-chainer/static/media/PokemonDPPt.0a8b1950.ttf"
+  },
+  {
+    "revision": "a8e045fdd423a4e0d7f4",
+    "url": "/egg-move-chainer/static/js/runtime~main.9bced9a8.js"
+  },
+  {
+    "revision": "de017b5be24a8b1221b3",
+    "url": "/egg-move-chainer/static/js/main.1e885245.chunk.js"
+  },
+  {
+    "revision": "7116a5d88d2a4293660d",
+    "url": "/egg-move-chainer/static/js/2.2d7aaafc.chunk.js"
+  },
+  {
+    "revision": "de017b5be24a8b1221b3",
+    "url": "/egg-move-chainer/static/css/main.b6e8c344.chunk.css"
+  },
+  {
+    "revision": "097490d7cb34b6e66004379c7bf84e54",
+    "url": "/egg-move-chainer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/egg-move-chainer/precache-manifest.3a409e474e96dea162a4cbdf4dc347ce.js"
+  "/egg-move-chainer/precache-manifest.3dfa4a8c4bdc190d81dcde3c8fb6ade1.js"
 );
 
 workbox.clientsClaim();
