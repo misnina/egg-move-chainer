22ac53be438a9fcaf24809045d980c0b97a5edff
Nina <ninarachaebuie@gmail.com>
2019-04-19 00:49:38 -0500

    Updates

---
+++ precache-manifest.25aa84d78ccb010ecf3ff60dbc902d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a8b1950a48a4ee96ba53ac7963beb5f",
+    "url": "/egg-move-chainer/static/media/PokemonDPPt.0a8b1950.ttf"
+  },
+  {
+    "revision": "a8e045fdd423a4e0d7f4",
+    "url": "/egg-move-chainer/static/js/runtime~main.9bced9a8.js"
+  },
+  {
+    "revision": "2be8ae56fbf80888d995",
+    "url": "/egg-move-chainer/static/js/main.d93bd180.chunk.js"
+  },
+  {
+    "revision": "15ea71b6f678634a3f94",
+    "url": "/egg-move-chainer/static/js/2.6f9976e6.chunk.js"
+  },
+  {
+    "revision": "2be8ae56fbf80888d995",
+    "url": "/egg-move-chainer/static/css/main.3caa343a.chunk.css"
+  },
+  {
+    "revision": "7165df0b9b3cef3267e13537e827ff62",
+    "url": "/egg-move-chainer/index.html"
+  }
+];
